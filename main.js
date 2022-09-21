ada1b132c90bd5e88e5be871c8b8a7e995c4648e
QodirjonAnorqulov <qodirjonanorqulov@gmail.com>
2022-09-21 14:47:06 +0500

    uyga vazifa 5

---
+++ main.js
@@ -0,0 +1,86 @@
+// const arr = [{
+//     name:"abdulla",
+//     age : 20,
+//     merried : false,
+//     score : 42,
+//     adress : "andijon"
+// },
+// {
+//     name:"Sardor",
+//     age : 21,
+//     merried : false,
+//     score : 55,
+//     adress : "chikago"
+// },
+// {
+//     name:"Davron",
+//     age : 22,
+//     merried : false,
+//     score : 22,
+//     adress : "parij"
+// },{
+//     name:"Tursun",
+//     age : 12,
+//     merried : false,
+//     score : 88,
+//     adress : "andijon"
+// },{
+//     name:"rustam",
+//     age : 17,
+//     merried : false,
+//     score : 99,
+//     adress : "amerika"
+// },{
+//     name:"timur",
+//     age : 55,
+//     merried : false,
+//     score : 45,
+//     adress : "avstraliya"
+// },{
+//     name:"vohid",
+//     age : 07,
+//     merried : false,
+//     score : 85,
+//     adress : "guliston"
+// },{
+//     name:"Nodir",
+//     age : 03,
+//     merried : false,
+//     score : 225,
+//     adress : "london"
+// },{
+//     name:"sarvar",
+//     age : 22,
+//     merried : false,
+//     score : 55,
+//     adress : "New york"
+// },{
+//     name:"hoshim",
+//     age : 11,
+//     merried : false,
+//     score : 75,
+//     adress : "toshkent"
+// }]
+// for(let i= 0; i<arr.length; i++){
+//     if( arr[i].score>=180){
+//         console.log(
+//         arr[i].name +" "+"imtixondan o'ttingiz"
+//             );
+//         }else{
+//             console.log(
+//               arr[i].name +" " + "qayta oqing"
+//                 );
+//     }
+// }
+         //    vazifa uyga
+// function optinal(attented,length){
+//     if(typeof attented , length ==="string" && attented ,length === "number" && attented,length >0 && attented>=length){
+//         const attented1 =Number(attented)
+//         const length1 = Number(length)
+//         console.log(attented1 ,length1);
+//     }else{
+//         console.log(false);
+//     }
+// }
+
+// console.log(optinal(55,"83"));
